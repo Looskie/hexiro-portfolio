0c0f1c775890822abfe20b88c58e84b97aec0052
devlooskie <hello@looskie.com>
2021-01-19 17:52:35 -0500

    LOLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL CYA

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
 const changeProjectFocus = (getProject) => {
   // getCurrentProjectFocus has to be inside the function or else it will have persistant data, weirdos ammirite.
   const getCurrentProjectFocus = document.querySelector('.active');
-
   // This is so I dont have to keep calling this annoying thing lol
   const getProjectElement = document.getElementById(getProject);
 
@@ -13,7 +12,6 @@ const changeProjectFocus = (getProject) => {
     // remove inactive and add active to project that is now active.
     getProjectElement.classList.remove('inactive');
     getProjectElement.classList.add('active');
-    
   }
 }
 
