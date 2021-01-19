b1253d40cec912b62377c15e2fc5e5ff660d4c0e
devlooskie <hello@looskie.com>
2021-01-19 16:13:12 -0500

    basic javascript kinda sorta

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,16 @@
+const changeProjectFocus = (getProject) => {
+  // getCurrentProjectFocus has to be inside the function or else it will have persistant data, weirdos ammirite.
+  const getCurrentProjectFocus = document.querySelector('.active');
+  // This is so I dont have to keep calling this annoying thing lol
+  const getProjectElement = document.getElementById(getProject);
+  // If the current project isn't already active
+  if (getCurrentProjectFocus.id != getProject) {
+    getCurrentProjectFocus.classList.remove('active');
+    getCurrentProjectFocus.classList.add('inactive')
+    getProjectElement.classList.remove('inactive');
+    getProjectElement.classList.add('active');
+  }
+}
 
 // Hexiros stupid PR he made
 console.log("splooge");
